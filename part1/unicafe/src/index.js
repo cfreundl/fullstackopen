3f9a385f5fa89b8b835240802ce8b274a8802190
Christoph Freundl <Christoph.Freundl@gmx.de>
2020-04-20 22:42:35 +0200

    Exercise 1.8
    unicafe step 3

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -9,23 +9,14 @@ const StatItem = ({ label, value }) => (
   <div>{label} {value}</div>
 )
 
-const App = () => {
-  // save clicks of each button to own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+const Statistics = ({ good, neutral, bad }) => {
   const all = good + neutral + bad
   const sum = good - bad
-
   const average = (sum, count) => count > 0 ? sum / count : 'N/A'
   const percentage = (value, count) => count > 0 ? 100 * value / count + ' %' : 'N/A'
-  
+
   return (
     <div>
-      <h1>give feedback</h1>
-      <Button text='good' handleClick={() => setGood(good + 1)} />
-      <Button text='neutral' handleClick={() => setNeutral(neutral + 1)} />
-      <Button text='bad' handleClick={() => setBad(bad + 1)} />
       <h1>statistics</h1>
       <StatItem label='good' value={good} />
       <StatItem label='neutral' value={neutral} />
@@ -33,6 +24,23 @@ const App = () => {
       <StatItem label='all' value={all} />
       <StatItem label='average' value={average(sum, all)} />
       <StatItem label='positive' value={percentage(good, all)} />
+    </div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button text='good' handleClick={() => setGood(good + 1)} />
+      <Button text='neutral' handleClick={() => setNeutral(neutral + 1)} />
+      <Button text='bad' handleClick={() => setBad(bad + 1)} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
